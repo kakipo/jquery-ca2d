e94c34798a66a8c5ff827d04a497d96f3baaa03c
kakipo <kakipo@gmail.com>
2013-02-16 08:18:35 +0900

    refactored added step method added samples

--- jquery.ca2d.js
+++ jquery.ca2d.js
@@ -1,157 +1,175 @@
 ;
 (function($) {
 
-  $.fn.ca2d = function(options) {
+  var methods = {
 
-    var defaults = {
-      cellSize: 10,
-      aliveColor: "#aaaaaa",
-      deadColor: "#ffffff",
-      fieldColor: "#ffffff",
-      color: function(state) {
-        return state == 1 ? "#aaaaaa" : "#ffffff";
-      },
-      rule: function(selfstate, neighborStates) {
-        var otherTotal = 0;
-        jQuery.each(neighborStates, function(idx, value) {
-          otherTotal += value;
-        });
+    init: function(options) {
 
-        var nextState = selfstate;
-        if(selfstate == 1) {
-          if(otherTotal <= 1 || otherTotal >= 4) {
-            nextState = 0;
-          }
-        } else {
-          if(otherTotal == 3) {
-            nextState = 1;
-          }
-        }
-        return nextState;
-      },
-      mutation: true
-    }
+      console.log("init");
 
-    // merge settings
-    var settings = $.extend({}, defaults, options);
+      var settings = $.extend({}, $.fn.ca2d.defaults, options);
 
-    return this.filter('canvas').each(function() {
-      $this = $(this);
-      if(!$this[0].getContext) {
-        return true; // continue
-      }
+      return this.filter('canvas').each(function() {
 
-      var cW = $this.width();
-      var cH = $this.height();
-      var grid = new Grid(cW, cH, settings.cellSize);
-      var ctx = this.getContext('2d');
-
-      step(ctx, grid);
-
-      // determine mobile / pc
-      var agent = navigator.userAgent;
-      if(agent.search(/iPhone/) != -1 || agent.search(/iPad/) != -1) {
-        $this.on("touchstart touchmove touchend", function(e) {
-          e.preventDefault();
-          step(ctx, grid);
-        });
-      } else {
-        $this.on("mousemove", function(e) {
-          step(ctx, grid);
-        });
-      }
-
-    });
-
-    function step(ctx, grid) {
-      updState(grid);
-      print(ctx, grid);
-    }
-
-    function updState(grid) {
-      // A B C
-      // D E F
-      // H I J
-      for(var i = 0; i < grid.rows; i++) {
-
-        var row = grid.cells[i];
-
-        for(var j = 0; j < grid.cols; j++) {
-
-          var cell = row[j]; // cell_E
-          var cell_A = grid.cells[i - 1 < 0 ? grid.rows - 1 : i - 1][j - 1 < 0 ? grid.cols - 1 : j - 1];
-          var cell_B = grid.cells[i - 1 < 0 ? grid.rows - 1 : i - 1][j];
-          var cell_C = grid.cells[i - 1 < 0 ? grid.rows - 1 : i - 1][j + 1 >= grid.cols ? 0 : j + 1];
-
-          var cell_D = grid.cells[i][j - 1 < 0 ? grid.cols - 1 : j - 1];
-          var cell_F = grid.cells[i][j + 1 >= grid.cols ? 0 : j + 1];
-
-          var cell_H = grid.cells[i + 1 >= grid.rows ? 0 : i + 1][j - 1 < 0 ? grid.cols - 1 : j - 1];
-          var cell_I = grid.cells[i + 1 >= grid.rows ? 0 : i + 1][j];
-          var cell_J = grid.cells[i + 1 >= grid.rows ? 0 : i + 1][j + 1 >= grid.cols ? 0 : j + 1];
-
-          cell.nextstate = settings.rule(cell.state, [cell_A.state, cell_B.state, cell_C.state, cell_D.state, cell_F.state, cell_H.state, cell_I.state, cell_J.state]);
-
-        } // end of for col
-      } // end of for row
-    } // end of updState
-
-    function print(ctx, grid) {
-
-      for(var i = 0; i < grid.rows; i++) {
-
-        var row = grid.cells[i];
-        var y = i * grid.cellSize;
-        for(var j = 0; j < grid.cols; j++) {
-
-          var x = j * grid.cellSize;
-
-          var cell = row[j];
-
-          ctx.fillStyle = settings.color(cell.nextstate);
-          ctx.fillRect(x, y, grid.cellSize, grid.cellSize);
-
-          // step futher
-          cell.state = cell.nextstate;
-        }
-      }
-    } // end of print
-
-    function Grid(x, y, cellSize) {
-
-      this.x = x;
-      this.y = y;
-      this.cellSize = cellSize;
-      this.rows = Math.floor(y / cellSize);;
-      this.cols = Math.floor(x / cellSize);;
-
-      var cells = new Array(this.rows);
-
-      for(var i = 0; i < this.rows; i++) {
-
-        var row = new Array(this.cols);
-
-        for(var j = 0; j < this.cols; j++) {
-
-          var state = Math.round(Math.random()); // 0 or 1
-          var color = settings.aliveColor;
-
-          row[j] = new Cell(i, j, color, state);
+        $this = $(this);
+        if(!$this[0].getContext) {
+          return true; // continue
         }
 
-        cells[i] = row;
-      }
+        $this.data('ca2d');
+        // If the plugin hasn't been initialized yet
+        if(!$this.data('ca2d')) {
+          var cW = $this.width();
+          var cH = $this.height();
+          var grid = new Grid(cW, cH, settings.cellSize);
+          var ctx = this.getContext('2d');
 
-      this.cells = cells;
+          $(this).data('ca2d', {
+            grid: grid,
+            context: ctx,
+            settings: settings
+          });
+        }
+        var data = $this.data('ca2d');
 
-      function Cell(x, y, color, state) {
+      });
+
+      function Grid(x, y, cellSize) {
+
         this.x = x;
         this.y = y;
-        this.color = color;
-        this.state = state;
-        this.nextstate = state;
-        this.extPeriod = 0;
-      } // end of Cell
-    } // end of grid
+        this.cellSize = cellSize;
+        this.rows = Math.floor(y / cellSize);;
+        this.cols = Math.floor(x / cellSize);;
+
+        var cells = new Array(this.rows);
+
+        for(var i = 0; i < this.rows; i++) {
+
+          var row = new Array(this.cols);
+
+          for(var j = 0; j < this.cols; j++) {
+
+            var state = Math.round(Math.random()); // 0 or 1
+            row[j] = new Cell(i, j, state);
+          }
+
+          cells[i] = row;
+        }
+
+        this.cells = cells;
+
+        function Cell(x, y, state) {
+          this.x = x;
+          this.y = y;
+          this.state = state;
+          this.nextstate = state;
+        } // end of Cell
+      } // end of Grid
+    },
+    // end of init
+
+    step: function() {
+      return this.filter('canvas').each(function() {
+
+        $this = $(this);
+        if(!$this.data('ca2d')) {
+          console.log("need to initialize ca2d");
+          return;
+        }
+        var data = $this.data('ca2d');
+
+        updState(data.grid, data.settings);
+        print(data.context, data.grid, data.settings);
+      });
+
+      function updState(grid, settings) {
+        // A B C
+        // D E F
+        // H I J
+        for(var i = 0; i < grid.rows; i++) {
+
+          var row = grid.cells[i];
+
+          for(var j = 0; j < grid.cols; j++) {
+
+            var cell = row[j]; // cell_E
+            var cell_A = grid.cells[i - 1 < 0 ? grid.rows - 1 : i - 1][j - 1 < 0 ? grid.cols - 1 : j - 1];
+            var cell_B = grid.cells[i - 1 < 0 ? grid.rows - 1 : i - 1][j];
+            var cell_C = grid.cells[i - 1 < 0 ? grid.rows - 1 : i - 1][j + 1 >= grid.cols ? 0 : j + 1];
+
+            var cell_D = grid.cells[i][j - 1 < 0 ? grid.cols - 1 : j - 1];
+            var cell_F = grid.cells[i][j + 1 >= grid.cols ? 0 : j + 1];
+
+            var cell_H = grid.cells[i + 1 >= grid.rows ? 0 : i + 1][j - 1 < 0 ? grid.cols - 1 : j - 1];
+            var cell_I = grid.cells[i + 1 >= grid.rows ? 0 : i + 1][j];
+            var cell_J = grid.cells[i + 1 >= grid.rows ? 0 : i + 1][j + 1 >= grid.cols ? 0 : j + 1];
+
+            cell.nextstate = settings.rule(cell.state, [cell_A.state, cell_B.state, cell_C.state, cell_D.state, cell_F.state, cell_H.state, cell_I.state, cell_J.state]);
+
+          } // end of for col
+        } // end of for row
+      } // end of updState
+
+      function print(ctx, grid, settings) {
+
+        for(var i = 0; i < grid.rows; i++) {
+
+          var row = grid.cells[i];
+          var y = i * grid.cellSize;
+          for(var j = 0; j < grid.cols; j++) {
+
+            var x = j * grid.cellSize;
+
+            var cell = row[j];
+
+            ctx.fillStyle = settings.color(cell.nextstate);
+            ctx.fillRect(x, y, grid.cellSize, grid.cellSize);
+
+            // step futher
+            cell.state = cell.nextstate;
+          }
+        }
+      } // end of print
+    },
+    update: function(content) {
+      console.log("update");
+    }
+  };
+
+  $.fn.ca2d = function(method) {
+    // Method calling logic
+    if(methods[method]) {
+      return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+    } else if(typeof method === 'object' || !method) {
+      return methods.init.apply(this, arguments);
+    } else {
+      $.error('Method ' + method + ' does not exist on jQuery.ca2d');
+    }
   }; // end of $.fn.ca2d
+
+  $.fn.ca2d.defaults = {
+    cellSize: 10,
+    color: function(state) {
+      return state == 1 ? "#aaaaaa" : "#ffffff";
+    },
+    rule: function(selfstate, neighborStates) {
+      var otherTotal = 0;
+      jQuery.each(neighborStates, function(idx, value) {
+        otherTotal += value;
+      });
+
+      var nextState = selfstate;
+      if(selfstate == 1) {
+        if(otherTotal <= 1 || otherTotal >= 4) {
+          nextState = 0;
+        }
+      } else {
+        if(otherTotal == 3) {
+          nextState = 1;
+        }
+      }
+      return nextState;
+    },
+  } // end of $.fn.ca2d.defaults
 })(jQuery);
